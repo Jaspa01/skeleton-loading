e137fe30f2cbe82f24c549e72a00d4c96f97689e
Jasper <adindechybke@gmail.com>
2024-01-07 04:54:39 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Skeleton from './components/Skeleton';
 
 function App() {
+
+  const [loading, isLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      isLoading(false);
+    }, 1000)
+  }, [])
+
   return (
     <div className="app">
-      <Card/>
-      <Skeleton/>
+      {loading ? <Skeleton /> : <Card />}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,56 @@
 import React from "react";
-import Watch from "../assets/w1.jpg";
+import Watch1 from "../assets/w1.jpg";
+import Watch2 from "../assets/w2.jpg";
+import Watch3 from "../assets/w3.jpg";
 
 const Card = () => {
   return (
-    <div className="card">
-      <div className="container">
-        <img
-          className="card-img"
-          src={Watch}
-          alt="/"
-        />
+    <>
+      <div className="card">
+        <div className="container">
+          <img className="card-img" src={Watch1} alt="/" />
+        </div>
+        <div>
+          <p className="text-1">Apple SmartWatch</p>
+          <p className="text-2">Price: $650</p>
+          <p className="text-3">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+            libero similique et consequuntur velit laudantium, a dolorem
+            reprehenderit? Quas, odit.
+          </p>
+        </div>
       </div>
-      <div>
-        <p className="text-1">Apple Headphones</p>
-        <p className="text-2">Price: $298</p>
-        <p className="text-3">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
-          libero similique et consequuntur velit laudantium, a dolorem
-          reprehenderit? Quas, odit.
-        </p>
+
+      <div className="card">
+        <div className="container">
+          <img className="card-img" src={Watch2} alt="/" />
+        </div>
+        <div>
+          <p className="text-1">Apple SmartWatch</p>
+          <p className="text-2">Price: $475</p>
+          <p className="text-3">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+            libero similique et consequuntur velit laudantium, a dolorem
+            reprehenderit? Quas, odit.
+          </p>
+        </div>
       </div>
-    </div>
+
+      <div className="card">
+        <div className="container">
+          <img className="card-img" src={Watch3} alt="/" />
+        </div>
+        <div>
+          <p className="text-1">Apple SmartWatch</p>
+          <p className="text-2">Price: $298</p>
+          <p className="text-3">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+            libero similique et consequuntur velit laudantium, a dolorem
+            reprehenderit? Quas, odit.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
